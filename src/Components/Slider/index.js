24ee33f095b7cce2cb7bdb5861c3d197d455dece
Ana julia <anajulia.anaju7@gmail.com>
2020-10-09 18:53:06 -0300

    melhorias

--- src/Components/Slider/index.js
+++ src/Components/Slider/index.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 
 import contador from "../../assets/imagens/contador.png";
+import gerador from '../../assets/imagens/gerador.png';
+import erro from '../../assets/imagens/404.png'
 import seta from '../../assets/imagens/left.svg';
 import direita from '../../assets/imagens/right.svg';
 
@@ -9,12 +11,19 @@ import './style.css'
 const projetos = [
     {
         imagem: `${contador}`,
-        descricao: 'Countador de clicks feito com React.js e JavaScript puro'
+        descricao: 'Countador de clicks feito com React.js e JavaScript',
+        url: 'https://counter-eight.vercel.app/'
     },
 
     {
-        imagem: `${''}`,
-        descricao: 'Currriculo digital feito com HTML e Css'
+        imagem: `${gerador}`,
+        descricao: 'Gerador de Mensagens feito com DOM ',
+        url: 'https://anaju7.github.io/Gerador-de-Mensagem/'
+    },
+    {
+        imagem: `${erro}`,
+        descricao: 'Pagina Erro404 feita com html e Css/Grid ',
+        url: 'https://anaju7.github.io/Erro404/'
     },
 ]
 
@@ -39,7 +48,7 @@ const Slider = () => {
                     <h1>Minhas Aplicações</h1>
                     <img className="img-projetos" src={dados[page].imagem}></img>
                     <p>{dados[page].descricao}</p>
-                    <a href="#" >Veja Agora</a>
+                    <a href={dados[page].url} target="_black" >Veja Agora</a>
                 </div>
             )}
             <div className='container-btn'>
